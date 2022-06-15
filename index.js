479dbba3df6ecc230905754f4a7abefec29804ac
finntolmie <finn.tolmie12@gmail.com>
2022-06-15 16:25:53 +1000

    Working etch-a-sketch

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+let CURRENT_NUM = 16;
 let grid = document.getElementById("grid");
 
 const createGrid = (num) => {
-    grid.innerHTML = "";
-    for (let i = 0; i < num; i++) {
-        for(let j = 0; j < num; j++) {
-            const element = document.createElement("div");
-            grid.append(element);
-            element.onmouseover = () => {
-                element.classList.add("touched");
-            }
-        }
+  if (num < 1 || num > 100) {
+    CURRENT_NUM = 16;
+    num = CURRENT_NUM;
+  }
+  grid.innerHTML = "";
+  console.log("cleared");
+  const root = document.querySelector(":root");
+  root.style.setProperty("--numRows", num);
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      const element = document.createElement("div");
+      grid.append(element);
+      element.onmouseover = () => {
+        element.classList.add("touched");
+      };
     }
-}
+  }
+};
 createGrid(16);
 
-let button = document.getElementById("new-grid");
-button.onclick = () => {
-    const newNum = prompt("How many squares per side?")
-    createGrid(newNum);
-}
+let newButton = document.getElementById("new-grid");
+newButton.onclick = () => {
+  CURRENT_NUM = prompt("How many squares per side? From 1 to 100");
+  createGrid(CURRENT_NUM);
+};
+
+let clearButton = document.getElementById("clear-grid");
+clearButton.onclick = () => {
+  createGrid(CURRENT_NUM);
+};
